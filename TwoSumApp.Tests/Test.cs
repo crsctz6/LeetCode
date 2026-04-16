f503ed895609a4d90dece184d544528d6c064186
crsctz6 <crs.usurelu@gmail.com>
2026-04-16 16:09:30 +0300

    Initial commit - Two Sum solution with tests

---
+++ TwoSumApp.Tests/Test.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSumApp;
+
+namespace TwoSumApp.Tests
+{
+    [TestClass]
+    public class TwoSumTests
+    {
+        [TestMethod]
+        public void TwoSum_BasicCase_ReturnsCorrectIndices()
+        {
+            var solution = new Solution();
+
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            var result = solution.TwoSum(nums, target);
+
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, result);
+        }
+
+        [TestMethod]
+        public void TwoSum_AnotherCase_ReturnsCorrectIndices()
+        {
+            var solution = new Solution();
+
+            int[] nums = { 3, 2, 4 };
+            int target = 6;
+
+            var result = solution.TwoSum(nums, target);
+
+            CollectionAssert.AreEqual(new int[] { 1, 2 }, result);
+        }
+
+        [TestMethod]
+        public void TwoSum_WithDuplicates()
+        {
+            var solution = new Solution();
+
+            int[] nums = { 3, 3 };
+            int target = 6;
+
+            var result = solution.TwoSum(nums, target);
+
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, result);
+        }
+    }
+}
---
+++ TwoSumApp/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TwoSumApp
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+
+                map[nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            var result = solution.TwoSum(nums, target);
+
+            Console.WriteLine($"[{result[0]}, {result[1]}]");
+        }
+    }
+}
